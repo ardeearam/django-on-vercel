b26fcd6d8667c88d947bf84ca6706fd55b1efeb5
Ardee Aram <ardee.aram@gmail.com>
2024-12-05 09:19:57 +0800

    Added the rocket welcome page, because it is more fun seeing that

--- example/views.py
+++ example/views.py
@@ -2,15 +2,8 @@
 from datetime import datetime
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+  now = datetime.now()
+  return render(request, 'example/index.html')
